9e9d429cd728e2ffc5c2cea626ccde5e60c3c28e
nancycrojas <nanclarisaok@gmail.com>
2022-06-26 19:31:56 -0300

    Funciones - ejercicio 04

---
+++ app.js
@@ -0,0 +1,15 @@
+//dividir(a, b)
+//Crear una función dividir que tome como argumentos dos números y devuelva la suma de ellos
+
+//dividir(12, 3) // 4
+//dividir(8, 4) // 2
+//sumar(30, 6) // 5
+
+const dividir = (a, b) => {
+//function dividir (a, b) {
+    return a / b;
+}
+
+dividir (12, 3);
+dividir(8, 4);
+dividir(30, 6);
